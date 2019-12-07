f82424300d95e856cab410c43b99b6e7cb0ef0e8
clara <clarisaharguindey@gmail.com>
2019-12-07 12:33:50 +0100

    Create project

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let seascape;
+
+function setup() {
+  createCanvas(500, 500);
+  createButton("+").mousePressed(generateSeascape);
+}
+
+function generateSeascape() {
+  const z = [];
+  for (let i = 0; i < 512; i++) {
+    z[i] = random(-1, 1);
+  }
+  const path = "http://localhost:8000/query";
+  const data = {
+    z: z,
+    truncation: 0.8
+  };
+  httpPost(path, "json", data, gotImage, gotError);
+}
+
+function gotError(error) {
+  console.error(error);
+}
+
+function gotImage(result) {
+  seascape = createImg(result.image);
+  seascape.hide();
+}
+
+function draw() {
+  background(220);
+  if (seascape) {
+    image(seascape, 0, 0);
+  }
+}
